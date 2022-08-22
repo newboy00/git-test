39b13301e95479b4d75919b7ec1bb90a1c84862e
zhystart <1059076621@qq.com>
2022-08-22 13:35:38 +0800

    second commit

--- src/main/java/com/zhystart/git/GitTset.java
+++ src/main/java/com/zhystart/git/GitTset.java
@@ -6,6 +6,6 @@ package com.zhystart.git;
 public class GitTset {
     public static void main(String[] args) {
         System.out.println("hello git!");
-//        System.out.println("hello git2!");
+        System.out.println("hello git2!");
     }
 }
