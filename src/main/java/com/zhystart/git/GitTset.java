c2d3a6a5a010191a135db0da1665b8bdea2bab85
zhystart <1059076621@qq.com>
2022-08-22 13:35:09 +0800

    first commit

---
+++ src/main/java/com/zhystart/git/GitTset.java
@@ -0,0 +1,11 @@
+package com.zhystart.git;
+
+/**
+ * @author zhystart
+ */
+public class GitTset {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+//        System.out.println("hello git2!");
+    }
+}
